3dacfbe4aead544fc24041ae9090131ed957b7da
mcrooks83 <57914909+mcrooks83@users.noreply.github.com>
2024-02-21 07:51:16 +0200

    add base Algorithm class

---
+++ classes/Algorithms.py
@@ -0,0 +1,21 @@
+
+
+class Algorithms:
+    def __init__(self, master, console, params, **kwargs):
+        super().__init__(master,  **kwargs)
+
+        self.console = console
+        self.params = params
+
+
+    def compute_loading_intensity(self):
+        pass
+
+        
+    
+
+
+
+        
+
+        
