f9b3a590e816d0eb2d5a4a06a927f2b86d456d68
mcrooks83 <57914909+mcrooks83@users.noreply.github.com>
2024-02-21 07:49:38 +0200

    add base classes

---
+++ classes/People.py
@@ -0,0 +1,22 @@
+
+
+# class to hold people
+
+class People:
+    def __init__(self, master, console, params, **kwargs):
+        super().__init__(master,  **kwargs)
+
+        self.console = console
+        self.params = params
+
+        
+def get_latest_assessment(self):
+    pass
+        
+    
+
+
+
+        
+
+        
---
+++ classes/TestResults.py
@@ -0,0 +1,20 @@
+
+
+class TestResult:
+    def __init__(self, master, console, params, **kwargs):
+        super().__init__(master,  **kwargs)
+
+        self.console = console
+        self.params = params
+
+        self.results = []
+        self.files = []
+
+        
+    
+
+
+
+        
+
+        
